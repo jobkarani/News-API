09f9af24f2964d808bdd41a8736d02e0a811c95d
job karani <job.nyambura@student.moringaschool.com>
2021-11-01 13:02:03 +0300

    registers a blueprint

---
+++ config.py
@@ -0,0 +1,20 @@
+import os 
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
